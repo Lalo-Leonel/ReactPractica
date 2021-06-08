638213c6e10fd201337b740657c588ede1ddc08a
Leonel <lionel.lalo@hotmail.com>
2021-06-07 21:31:59 -0500

    Segundo commit

--- src/App.js
+++ src/App.js
@@ -1,31 +1,97 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Saludo from './Saludo';
+
 import Navigation from './components/Navigation';
  
 import { todos } from './todos.json';
+import TodoForm from './components/TodoForm';
+
 console.log(todos);
 
 
-function App() {
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      todos
+    }
+    this.handleAddTodo = this.handleAddTodo.bind(this);
+  }
+
+  removeTodo(index) {
+    this.setState({
+      todos: this.state.todos.filter((e, i) => {
+        return i !== index
+      })
+    });
+  }
+
+  handleAddTodo(todo){
+    this.setState({
+      todos: [...this.state.todos, todo]
+    })
+  }
+
+  render(){
+    const todos = this.state.todos.map((todo, i) => {
+      return (
+        <div className="col-md-4" key={i}>
+          <div className="card mt-4">
+            <div className="card-title text-center">
+              <h3>{todo.title}</h3>
+              <span className="badge badge-pill badge-danger ml-2">
+                {todo.priority}
+              </span>
+            </div>
+            <div className="card-body">
+              {todo.description}
+            </div>
+            <div className="card-footer">
+              <button
+                className="btn btn-danger"
+                onClick={this.removeTodo.bind(this, i)}>
+                Delete
+              </button>
+            </div>
+          </div>
+        </div>
+      )
+    });
+
+    return (
+      <div className="App">
+          <nav className="navbar navbar-dark bg-dark">
+            <a href="" className="text-white">
+              Task
+              <span className="badge badge-pill badge-light ml-2">
+                5
+              </span>
+            </a>
+                  
+          </nav>
+          <div className="container">
+            <div className="row mt-4">
+              <div className="col-md-3">
+                <img src={logo} className="App-logo" alt="logo" />
+                <TodoForm/>
+              </div> 
+              <div className="col-md-9">
+                <div className="row">
+                  <Navigation/>
+                </div>
+              </div>
+            </div> 
+            
+          </div>       
+          
+      </div>
+    );
+
+  }
+
   
-  return (
-    <div className="App">
-        <nav className="navbar navbar-dark bg-dark">
-          <a href="" className="text-white">
-            Task
-            <span className="badge badge-pill badge-light ml-2">
-              5
-            </span>
-          </a>
-                
-        </nav>
-        <Navigation/>
-        <img src={logo} className="App-logo" alt="logo" />
-        
-        
-    </div>
-  );
 }
 
 export default App;
